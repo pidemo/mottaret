e67fb122368a2050056fd8624c568ef2e7b66caf
Pierre de Montalte <pierredemontalte@gmail.com>
2023-10-04 02:07:53 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,34 +1,34 @@
 import './styles/style.css'
 
-console.log('code is live');
+var currentDate = new Date();
+var currentMonth = currentDate.getMonth() + 1; // January is 0
+let isSummer;
 
-document.addEventListener("DOMContentLoaded", function() {
-    var currentDate = new Date();
-    var currentMonth = currentDate.getMonth() + 1; // January is 0
-
-    if (currentMonth >= 3 && currentMonth <= 9) {
-        // Show elements for January to June
+  if (currentMonth >= 3 && currentMonth <= 9) {
+        // Show elements for March to September included
         isSummer = true;
     } else {
-        // Show elements for July to December
+        // Show elements for October to March
         isSummer = false;
     };
-    
-    console.log(isSummer);
-    
-    
-    
 // Season Switcher
 let seasonSwitches = document.querySelectorAll('.season_switcher');
+
 if (isSummer) {
-	seasonSwitches.forEach.classList.add(".is-summer")
+    seasonSwitches.forEach(function(element) {
+        element.classList.add('is-summer');
+        element.setAttribute("data-current-season", "summer");
+    });
+} else {
+    seasonSwitches.forEach(function(element) {
+        element.classList.remove('is-summer');
+        element.setAttribute("data-current-season", "winter");
+    });
 };
+
+
 let summerStuff = document.querySelectorAll('[data-season="summer"]');
 let winterStuff = document.querySelectorAll('[data-season="winter"]');
-
-// to remove
-let initialSeason = seasonSwitches[0].getAttribute('data-current-season');
-let seasonSwitch = document.getElementById('season-switch');
 
 // initial show/hide elements based on current season
 if (isSummer) {
@@ -47,28 +47,24 @@ if (isSummer) {
     });
 };
 
-// change visibility on click
 for (var i = 0; i < seasonSwitches.length; i++) {
-	seasonSwitches[i].addEventListener('click', function() {
-		// get current season
-    let currentSeason = seasonSwitch.getAttribute('data-current-season');
-    
-    // switch visibility on all seasonal items
-    summerStuff.forEach(function(element) {
-      element.classList.toggle('is-hidden');
+    seasonSwitches[i].addEventListener('click', function() {
+        
+        // add or remove is-summer class on all switchers
+        seasonSwitches.forEach(function(element) {
+                element.classList.toggle('is-summer');
+                //element.setAttribute("data-current-season", "summer");
+        });
+        //console.log('before value reset');
+        //let isSummer = true;
+        //console.log('after value reset');
+        // switch visibility on all seasonal items
+        summerStuff.forEach(function(element) {
+            element.classList.toggle('is-hidden');
+        });
+        winterStuff.forEach(function(element) {
+            element.classList.toggle('is-hidden');
+        });
+        console.log(isSummer);
     });
-    winterStuff.forEach(function(element) {
-    	element.classList.toggle('is-hidden');
-    });
-    
-    //switch current season
-    if (currentSeason == "summer") {
-    	seasonSwitch.setAttribute("data-current-season", "winter");
-    } else {
-    	seasonSwitch.setAttribute("data-current-season", "summer");
-    };
-    
- });
 };
-    
-});
